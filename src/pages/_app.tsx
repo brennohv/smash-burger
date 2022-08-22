13d8d27f41fc2d5906854e58b1867ed35b995a14
brennohv <brennovicentini@gmail.com>
2022-08-22 02:20:26 +0100

    Fix shortcut

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ function App({ Component, pageProps }: AppProps) {
     <ThemeProvider theme={theme}>
       <Head>
         <title>Smash Burger PT</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="shortcut icon" href="img/logo sem fundo.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
