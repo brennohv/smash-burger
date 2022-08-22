8eaa8b983d39421511f837c1599d63cbde5b8a02
brennohv <brennovicentini@gmail.com>
2022-08-22 02:22:05 +0100

    Update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,10 +14,7 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
-        <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-        />
+        <meta name="Smash Burger" content="The best burger of Lisbon" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
