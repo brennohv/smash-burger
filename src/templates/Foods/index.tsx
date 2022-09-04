535a7ae9a76256675cfbfda6d2442fa713337490
brennohv <brennovicentini@gmail.com>
2022-09-04 15:01:09 +0100

    remove ios pwa

--- src/templates/Foods/index.tsx
+++ src/templates/Foods/index.tsx
@@ -9,7 +9,6 @@ import Footer from 'components/Footer'
 
 import { CategoriesProps } from 'types/api'
 import { Fragment, useState } from 'react'
-// import PwaInstallPopupIOS from 'react-pwa-install-ios'
 
 const FoodsTemplate = ({ categories }: CategoriesProps) => {
   const [englishLang, setEnglishLang] = useState(false)
@@ -44,12 +43,6 @@ const FoodsTemplate = ({ categories }: CategoriesProps) => {
         ))}
       </Container>
       <Footer></Footer>
-
-      {/* <PwaInstallPopupIOS
-        delay={3}
-        lang="pt"
-        appIcon="/img/logo.jpg"
-      ></PwaInstallPopupIOS> */}
     </S.Wrapper>
   )
 }
