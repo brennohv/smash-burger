a4f0684caf50210f686034baf9bfd66b93b9c0b9
brennohv <brennovicentini@gmail.com>
2022-09-04 23:26:37 +0100

    remove teste localStorage pwa

--- src/components/PwaIos/index.tsx
+++ src/components/PwaIos/index.tsx
@@ -1,12 +1,19 @@
 import * as S from './styles'
 import PwaInstallPopupIOS from 'react-pwa-install-ios'
+import { useEffect } from 'react'
 
-const PwaIos = () => (
-  <PwaInstallPopupIOS
-    delay={3}
-    lang="pt"
-    appIcon="/img/logo.jpg"
-  ></PwaInstallPopupIOS>
-)
+const PwaIos = () => {
+  useEffect(() => {
+    window.localStorage.removeItem('pwa_popup_display')
+  }, [])
+
+  return (
+    <PwaInstallPopupIOS
+      delay={3}
+      lang="pt"
+      appIcon="/img/logo.jpg"
+    ></PwaInstallPopupIOS>
+  )
+}
 
 export default PwaIos
