2f8ab0a885948be26fc92a5ee4ff9e50b95143d3
brennohv <brennovicentini@gmail.com>
2022-11-25 22:45:12 +0000

    add static categories in project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import client from 'graphql/client'
-import GET_CATEGORIES from 'graphql/queries/getCategories'
+// import client from 'graphql/client'
+// import GET_CATEGORIES from 'graphql/queries/getCategories'
 import FoodsTemplate from 'templates/Foods'
+import { allProducts } from 'templates/Foods/mock'
 import { CategoriesProps } from 'types/api'
 
 export default function Home({ categories }: CategoriesProps) {
@@ -8,9 +9,8 @@ export default function Home({ categories }: CategoriesProps) {
 }
 
 export async function getStaticProps() {
-  const { categories } = await client.request(GET_CATEGORIES)
-
-  // console.log(categories)
+  // const { categories } = await client.request(GET_CATEGORIES)
+  const { categories } = allProducts
 
   return {
     props: {
--- src/templates/Foods/mock.ts
+++ src/templates/Foods/mock.ts
@@ -1,79 +1,245 @@
-export const data = [
-  {
-    title: 'Super Bacon',
-    description:
-      'Pão brioche, 2 x smashed burger bovino 80g, 2 x queijo cheddar, bacon e maionese de bacon. Acompanha batata palito crocante.',
-    src: '/img/Baacon.jpg',
-    price: 11.5
-  },
-  {
-    title: 'Super Bacon',
-    description:
-      'Pão brioche, 2 x smashed burger bovino 80g, 2 x queijo cheddar, bacon e maionese de bacon. Acompanha batata palito crocante.',
-    src: '/img/Baacon.jpg',
-    price: 11.5
-  },
-  {
-    title: 'Super Bacon',
-    description:
-      'Pão brioche, 2 x smashed burger bovino 80g, 2 x queijo cheddar, bacon e maionese de bacon. Acompanha batata palito crocante.',
-    src: '/img/Baacon.jpg',
-    price: 11.5
-  },
-  {
-    title: 'Super Bacon',
-    description:
-      'Pão brioche, 2 x smashed burger bovino 80g, 2 x queijo cheddar, bacon e maionese de bacon. Acompanha batata palito crocante.',
-    src: '/img/Baacon.jpg',
-    price: 11.5
-  },
-  {
-    title: 'Super Bacon',
-    description:
-      'Pão brioche, 2 x smashed burger bovino 80g, 2 x queijo cheddar, bacon e maionese de bacon. Acompanha batata palito crocante.',
-    src: '/img/Baacon.jpg',
-    price: 11.5
-  },
-  {
-    title: 'Super Bacon',
-    description:
-      'Pão brioche, 2 x smashed burger bovino 80g, 2 x queijo cheddar, bacon e maionese de bacon. Acompanha batata palito crocante.',
-    src: '/img/Baacon.jpg',
-    price: 11.5
-  },
-  {
-    title: 'Super Bacon',
-    description:
-      'Pão brioche, 2 x smashed burger bovino 80g, 2 x queijo cheddar, bacon e maionese de bacon. Acompanha batata palito crocante.',
-    src: '/img/Baacon.jpg',
-    price: 11.5
-  },
-  {
-    title: 'Super Bacon',
-    description:
-      'Pão brioche, 2 x smashed burger bovino 80g, 2 x queijo cheddar, bacon e maionese de bacon. Acompanha batata palito crocante.',
-    src: '/img/Baacon.jpg',
-    price: 11.5
-  },
-  {
-    title: 'Super Bacon',
-    description:
-      'Pão brioche, 2 x smashed burger bovino 80g, 2 x queijo cheddar, bacon e maionese de bacon. Acompanha batata palito crocante.',
-    src: '/img/Baacon.jpg',
-    price: 11.5
-  },
-  {
-    title: 'Super Bacon',
-    description:
-      'Pão brioche, 2 x smashed burger bovino 80g, 2 x queijo cheddar, bacon e maionese de bacon. Acompanha batata palito crocante.',
-    src: '/img/Baacon.jpg',
-    price: 11.5
-  },
-  {
-    title: 'Super Bacon',
-    description:
-      'Pão brioche, 2 x smashed burger bovino 80g, 2 x queijo cheddar, bacon e maionese de bacon. Acompanha batata palito crocante.',
-    src: '/img/Baacon.jpg',
-    price: 11.5
-  }
-]
+export const allProducts = {
+  categories: [
+    {
+      name: 'Menu',
+      id: '9',
+      englishName: 'Menu',
+      products: [
+        {
+          title: 'Smash Super Bacon',
+          id: '28',
+          englishTitle: 'Smash Super Bacon',
+          description:
+            'Pão brioche, 2 x smashed burger bovino 80g, 2 x queijo cheddar, bacon e maionese de bacon. Acompanha batata palito crocante e bebida.',
+          englishDescription:
+            'Brioche bun, 2 x smashed burger 80 g, 2 x cheddar cheese, bacon and bacon mayo. Served with fries and drink.',
+          price: 8.7,
+          image: {
+            url: 'https://res.cloudinary.com/won-games-api/image/upload/v1661114315/Smash_Bacon_407b1e5f01.jpg'
+          }
+        },
+        {
+          title: 'Smash Crispy Onion',
+          id: '29',
+          englishTitle: 'Smash Crispy Onion',
+          description:
+            'Pão brioche, 2 x smashed burger bovino 80g, 2 x queijo cheddar, cebola crispy e maionese de bacon. Acompanha batata palito crocante e bebida.',
+          englishDescription:
+            'Brioche bun, 2 x smashed burger 80g, 2 x cheddar cheese, crispy onion and bacon mayo. Served with fries and drink.',
+          price: 8.7,
+          image: {
+            url: 'https://res.cloudinary.com/won-games-api/image/upload/v1661128307/E0_E66337_75_DA_4_F1_A_8_BA_7_D5_FD_8_C26_B34_D_273c89bbb0.jpg'
+          }
+        },
+        {
+          title: 'Smash Chicken',
+          id: '30',
+          englishTitle: 'Smash Chicken',
+          description:
+            'Pão brioche, peito de frango empanado, queijo mozzarella, rúcula, maionese de bacon. Acompanha batata palito crocante e bebida.',
+          englishDescription:
+            'Brioche bun, breaded chicken breast, mozzarella cheese, arugula, bacon mayo. Served with fries and drink.',
+          price: 8.4,
+          image: {
+            url: 'https://res.cloudinary.com/won-games-api/image/upload/v1661128312/F3498651_5769_481_D_91_B2_8854055_A218_F_d8faf9ee84.jpg'
+          }
+        },
+        {
+          title: 'Smash Egg',
+          id: '31',
+          englishTitle: 'Smash Egg',
+          description:
+            'Pão brioche, 2 x smashed burger bovino 80g, 2 x queijo cheddar, ovo e maionese smash. Acompanha batata palito crocante e bebida.',
+          englishDescription:
+            'Brioche bun, 2 x smashed burger 80g, 2 x cheddar cheese, egg and smash mayo. Served with fries and drink.',
+          price: 8.5,
+          image: {
+            url: 'https://res.cloudinary.com/won-games-api/image/upload/v1661128307/05_CD_65_D3_D0_F5_42_CA_8_C9_A_A0741_A93_AE_7_D_09fa2a7dc0.jpg'
+          }
+        },
+        {
+          title: 'Original Salad',
+          id: '32',
+          englishTitle: 'Original Salad',
+          description:
+            'Pão brioche, 2 x smashed burger bovino 80g, 2 x queijo cheddar, alface iceberg, tomate, picles, cebola roxa e maionese smash. Acompanha batata palito crocante e bebida.',
+          englishDescription:
+            'Brioche bun, 2 x smashed burger 80g, 2 x cheddar cheese, iceberg lettuce, tomato, pickle, red onion and smash mayo. Served with fries and drink.',
+          price: 8.5,
+          image: {
+            url: 'https://res.cloudinary.com/won-games-api/image/upload/v1661288536/Smash_Burger_11_0f3d971b21.jpg'
+          }
+        },
+        {
+          title: 'The Original',
+          id: '33',
+          englishTitle: 'The Original',
+          description:
+            'Pão brioche, 2 x smashed burger bovino 80g, 4 x queijo cheddar e maionese smash. Acompanha batata palito crocante e bebida.',
+          englishDescription:
+            'Brioche bun, 2 x smashed burger 80g, 4 x cheddar cheese and smash mayo. Served with fries and drink.',
+          price: 8.2,
+          image: {
+            url: 'https://res.cloudinary.com/won-games-api/image/upload/v1661288536/Smash_Burger_10_1bcbbf2fb8.jpg'
+          }
+        },
+        {
+          title: 'Smash Veggie',
+          id: '34',
+          englishTitle: 'Smash Veggie',
+          description:
+            'Pão brioche, veggie burger, queijo brie, cogumelos, espinafre salteado e maionese smash. Acompanha batata palito crocante e bebida.',
+          englishDescription:
+            'Brioche bun, veggie burger, brie cheese, mushrooms, sautéed spinach and smash mayo. Served with fries and drink.',
+          price: 9.9,
+          image: {
+            url: 'https://res.cloudinary.com/won-games-api/image/upload/v1661128306/125_F9969_38_AB_4275_BD_08_014477_ADBF_3_D_3bc739be4a.jpg'
+          }
+        },
+        {
+          title: 'Smashinho',
+          id: '35',
+          englishTitle: 'Smashinho',
+          description:
+            'Pão brioche, smashed burger bovino 80g, queijo cheddar e maionese smash. Acompanha batata palito crocante e bebida.',
+          englishDescription:
+            'Brioche bun, 80g smashed burger, cheddar cheese and smash mayo. Served with fries and drink.',
+          price: 5,
+          image: {
+            url: 'https://res.cloudinary.com/won-games-api/image/upload/v1661289645/smashinho_d1ab4d9427.jpg'
+          }
+        }
+      ]
+    },
+    {
+      name: 'Entradas',
+      id: '10',
+      englishName: 'Snacks',
+      products: [
+        {
+          title: 'Batata Supreme',
+          id: '36',
+          englishTitle: 'Supreme Fries',
+          description: 'Batata frita acompanhada com cheddar cremoso e bacon',
+          englishDescription: 'Fries with creamy cheddar and bacon',
+          price: 4.5,
+          image: {
+            url: 'https://res.cloudinary.com/won-games-api/image/upload/v1661288958/efe5d062_0763_481a_aed0_6ac03feff384_58eaf98468.jpg'
+          }
+        },
+        {
+          title: 'Chicken Wings Crocante',
+          id: '37',
+          englishTitle: 'Chicken Wings Crocante',
+          description: '5 unidades, acompanha maionese smash',
+          englishDescription: '5 units, with smash mayo',
+          price: 4.2,
+          image: {
+            url: 'https://res.cloudinary.com/won-games-api/image/upload/v1661128306/7_C4018_B9_C90_B_4_DD_0_9230_B5_B12_D6_D8_E03_09c2c0f826.jpg'
+          }
+        },
+        {
+          title: 'Nacho Chesse',
+          id: '38',
+          englishTitle: 'Nacho Chesse',
+          description: ' 6 unidades, acompanha maionese smash',
+          englishDescription: '6 units, with smash mayo',
+          price: 3.9,
+          image: {
+            url: 'https://res.cloudinary.com/won-games-api/image/upload/v1661289819/Smash_Burger_3_3c433eb7f2.jpg'
+          }
+        },
+        {
+          title: 'Finger de Frango',
+          id: '39',
+          englishTitle: 'Chicken Finger',
+          description: '4 unidades, acompanha maionese smash',
+          englishDescription: '4 units, with smash mayo',
+          price: 3.9,
+          image: {
+            url: 'https://res.cloudinary.com/won-games-api/image/upload/v1661128311/B2024_C6_E_D2_B9_4912_B5_F5_9_EE_62_DEA_93_E8_a78b528814.jpg'
+          }
+        },
+        {
+          title: 'Spicy Wings',
+          id: '40',
+          englishTitle: 'Spicy Wings',
+          description: '6 unidades, acompanha maionese smash',
+          englishDescription: '6 units, with smash mayo',
+          price: 3.9,
+          image: {
+            url: 'https://res.cloudinary.com/won-games-api/image/upload/v1661128311/5866148_C_E72_F_48_AD_9741_950_D9_AC_811_DA_84c97e8c09.jpg'
+          }
+        },
+        {
+          title: 'Batata Palito',
+          id: '41',
+          englishTitle: 'French fries',
+          description: 'Acompanha maionese smash',
+          englishDescription: 'With Smash mayo',
+          price: 2,
+          image: {
+            url: 'https://res.cloudinary.com/won-games-api/image/upload/v1661288882/Smash_Burger_5_ae4fd7c08c.jpg'
+          }
+        },
+        {
+          title: 'Batata Doce',
+          id: '42',
+          englishTitle: 'Sweet Fries',
+          description: 'Acompanha maionese smash',
+          englishDescription: 'With Smash mayo',
+          price: 2.5,
+          image: {
+            url: 'https://res.cloudinary.com/won-games-api/image/upload/v1661288471/F59_DD_276_818_C_4_AFB_8_BE_8_2_DB_425_BF_3431_33cbb51fa2.jpg'
+          }
+        }
+      ]
+    },
+    {
+      name: 'Trio poupança ',
+      id: '11',
+      englishName: 'Single',
+      products: [
+        {
+          title: 'Texas',
+          id: '43',
+          englishTitle: 'Texas',
+          description:
+            'Pão brioche, smashed burger bovino 80g, picles, ceba roxa, ketchup e maionese smash.',
+          englishDescription:
+            'Brioche bun, 80g smashed burger, pickles, red onion, ketchup and smash mayo.',
+          price: 3,
+          image: {
+            url: 'https://res.cloudinary.com/won-games-api/image/upload/v1661289171/Texas_25b0ef5f35.png'
+          }
+        },
+        {
+          title: 'Miami',
+          id: '44',
+          englishTitle: 'Miami',
+          description:
+            'Pão brioche, smashed burger bovino 80g, queijo cheddar, picles, ceba roxa, ketchup e maionese smash.',
+          englishDescription:
+            'Brioche bun, 80g smashed burger, cheddar cheese, pickles, red onion, ketchup and smash mayo.',
+          price: 3.7,
+          image: {
+            url: 'https://res.cloudinary.com/won-games-api/image/upload/v1661289090/Miami_b60ccf5e64.png'
+          }
+        },
+        {
+          title: 'California',
+          id: '46',
+          englishTitle: 'California',
+          description:
+            'Pão brioche, smashed burger bovino 80g, queijo cheddar, alface iceberg, tomate, ketchup e maionese smash',
+          englishDescription:
+            'Brioche bun, 80g smashed burger, cheddar cheese, iceberg lettuce, tomato, ketchup and smash mayo.',
+          price: 4,
+          image: {
+            url: 'https://res.cloudinary.com/won-games-api/image/upload/v1661289130/California_155a51330d.png'
+          }
+        }
+      ]
+    }
+  ]
+}
